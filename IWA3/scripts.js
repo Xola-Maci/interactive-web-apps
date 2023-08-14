604fc11b3cb639df6a7aa00ed6079dd08ff766b5
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-08-14 14:00:37 +0200

    fixed

--- IWA3/scripts.js
+++ IWA3/scripts.js
@@ -3,4 +3,4 @@ import {role as nwabisa} from './nwabisa.js';
 import {role as johanes} from './johannes.js';
 import {role as alex} from './alex.js';
 
-console.log('Roles:', nwabisa, johanes, alex)
+console.log('Roles:', nwabisa,"," ,johanes,",", alex)
