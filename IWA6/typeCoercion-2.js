6ebad89abc2b63b6054a004d24d8328d76262baa
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-08-14 14:43:30 +0200

    completed challenge 2 of IWA6

--- IWA6/typeCoercion-2.js
+++ IWA6/typeCoercion-2.js
@@ -3,16 +3,20 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 0;
+const hourOfDay = 0; //fixed the value from 00 to 0 as it gave an error
 const minuteOfDay = 0;
 
 // Only change below this line
-
-if (hourOfDay && minuteOfDay !== null && hourOfDay == '00' && minuteOfDay == '00') {
- const taxAsDecimal = tax / '100';
- const startingAfterTax = salary * '1' - taxAsDecimal;
- const balance = startingAfterTax - transport - food - rent;
  
+
+if (hourOfDay == 0 && minuteOfDay == 0) {
+ const taxAsDecimal = parseFloat(tax)/ 100; // converts tax into tax decimal value
+ const startingAfterTax = salary * (1 - taxAsDecimal); // enclosed 1 - taxAsDecimal with () to fix the calculatio
+ const balance = startingAfterTax - transport - food - rent;
+ console.log('R', balance.toFixed(2));
+}else{
+  console.log('Balance can only be calculated at midnight.');
 }
 
-console.log(balance.toFixed(2));
+
+//console.log('R', balance.toFixed(2));
