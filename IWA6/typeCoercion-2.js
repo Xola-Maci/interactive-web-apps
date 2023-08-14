6f368e8e1c1c48fea52e2ea716146b0e22882486
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-08-14 15:25:37 +0200

    changed value back to 00

--- IWA6/typeCoercion-2.js
+++ IWA6/typeCoercion-2.js
@@ -3,13 +3,13 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 0; //fixed the value from 00 to 0 as it gave an error
-const minuteOfDay = 0;
+const hourOfDay = 00; 
+const minuteOfDay = 00;
 
 // Only change below this line
  
 
-if (hourOfDay == 0 && minuteOfDay == 0) {
+if (hourOfDay == 00 && minuteOfDay == 00) {
  const taxAsDecimal = parseFloat(tax)/ 100; // converts tax into tax decimal value
  const startingAfterTax = salary * (1 - taxAsDecimal); // enclosed 1 - taxAsDecimal with () to fix the calculatio
  const balance = startingAfterTax - transport - food - rent;
