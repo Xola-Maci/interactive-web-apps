4cc46578a72668ac3acd98004660d9cfd56aa2f2
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-08-16 11:41:15 +0200

    fixed problems with validation

--- IWA7/secondChallenge.js
+++ IWA7/secondChallenge.js
@@ -1,17 +1,14 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
-let user = firstname; //declared a variable to either firstname or nickname
+//let user = firstname; 
 
-if(user === firstname){ 
-    //conditioned the variable to log the firstname is user is set to the firstname
+if(nickname){ 
+    console.log(`Good Morning, ${nickname}`);
+}else if(firstname){ 
     console.log(`Good Morning ${firstname}`);
-}else if(user === nickname){ 
-    //conditioned the variable to log the nickname is user is set to the nickname
-    console.log(`Good Morning ${nickname}`);
+}else{
+    console.log(`Good Morning`);
 }
 
-if(user != firstname && user !=nickname){
-    console.log(`Good Morning`);
-}//conditioned the console to log only Goodmorning if neither nickname and firstname are set as user.
-
 //console.log("Good Morning, ${nickname} || {firstname}!")
+
