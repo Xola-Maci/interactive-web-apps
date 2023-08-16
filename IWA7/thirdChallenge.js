408272994ffc85c502320891772f1e1bc117b154
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-08-16 11:11:26 +0200

    fixing spacing

--- IWA7/thirdChallenge.js
+++ IWA7/thirdChallenge.js
@@ -11,14 +11,9 @@ const divider = '----------------------------------'
 // Only change below this line
 
 const owed = `R ${parseFloat(-leoBalance -sarahBalance).toFixed(2)}`
-const leo = `${leoName} ${leoSurname}(Owed: R ${parseInt(-leoBalance).toFixed(2)})`
-const sarah = `${sarahName}${sarahSurname} (Owed: R ${parseFloat(-sarahBalance).toFixed(2)})`
+const leo = `${leoName} ${leoSurname.trim(" ")} (Owed: R ${parseFloat(-leoBalance).toFixed(2)})`
+const sarah = `${sarahName.trim(" ")} ${sarahSurname} (Owed: R ${parseFloat(-sarahBalance).toFixed(2)})`
 const total = "Total amount owed: "
-const result = `${leo} 
-${sarah} 
-
-${divider} 
-${total} ${owed} 
-${divider}`
+const result = `\n${leo}\n${sarah}\n\n${divider}\n\n${total}${owed}\n${divider}`
 
 console.log(result)
