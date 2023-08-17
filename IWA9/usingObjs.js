7d70e12ece218091c6406ec35daf94bfcb5ffda7
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-08-17 12:57:46 +0200

    Cmpleted challenge 9

--- IWA9/usingObjs.js
+++ IWA9/usingObjs.js
@@ -5,19 +5,28 @@ const size = 'large'
 
 // Only change the syntax below (not the values or key names)
 
+/**
+ * List of expenses
+ */
 const expenses = {
     food: 51.7501,
     transport:  10.2,
 }
   
+/**
+ * The different type of tax that will help to determine the salary after it has been deducted.
+ */
 const tax = {
-    734: '3%',
-    234: '20%',
-    913: '12%',
-    415: '38%',
-    502: '42%',
+    '734': '3%',
+    '234': '20%',
+    '913': '12%',
+    '415': '38%',
+    '502': '42%',
 }
 
+/**
+ * Rent amount for different apartments and houses
+ */
 const rent = {
     none: 0,
     'small-room': 200,
@@ -30,8 +39,8 @@ const rent = {
 
 // You can change below however you want
 
-const taxAsDecimal = tax.913 / 100
-const startingAfterTax = salary * 1 - taxAsDecimal
-const type = lodging + size
-const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
-console.log(balance)
+const taxAsDecimal =parseFloat(tax['913']) / 100;
+const startingAfterTax = salary * (1 - taxAsDecimal);
+const type = `${size}-${lodging}`
+const balance = (startingAfterTax - (expenses.transport) - (expenses.food) - rent[type]).toFixed(2) 
+console.log(`R ${balance}`)
