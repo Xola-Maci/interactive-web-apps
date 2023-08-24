fa708e0f413877d9acb40b416fd8d1500bd1cf6a
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-08-24 13:57:36 +0200

    added comments

--- IWA13/iwa13_challenge1.js
+++ IWA13/iwa13_challenge1.js
@@ -5,20 +5,20 @@ let calculated = '1'
 // Only allowed to change below
 
 
-const  logCalc = () => { 
-    const isString = typeof calculated === 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseInt(calculated)
-    calculated = calculatedAsNumber + 1 
+const  logCalc = () => { // removed = and placed it after function name to create an arrow function
+    const isString = typeof calculated === 'numerical-string' // added === to check if true
+    const calculatedAsNumber = isString ? calculated : parseInt(calculated) // changed parseNumber to parseInt to convert string to an integer
+    calculated = calculatedAsNumber + 1 // removed === to = to show the value is being assigned to a variable
 }
 
-const  calcUser = () => {
-  logCalc();
+const  calcUser = () => { // removed = and placed it after function name to create an arrow function
+  logCalc() // added () to show that the function is being called
   if (calculated > 2) user = 'John'
   if (calculated > 2) state = 'requesting'
   if (calculated > 3) state = 'idle'
 }
 
-const  checkUser = () => {
+const  checkUser = () => { // removed = and placed it after function name to create an arrow function
 	if (user && state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
 	}
