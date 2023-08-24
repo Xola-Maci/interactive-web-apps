1d4f4b3a1f503e21e9bde42b2bb2ffa031f312c8
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-08-24 13:51:57 +0200

    completed challenge iwa 13

--- LessonCodeTest/scriptsjs.js
+++ LessonCodeTest/scriptsjs.js
@@ -36,3 +36,42 @@ const changeCase = (text, format) => {
 console.log(changeCase('Xola', 'uppercase'))
 console.log(changeCase('Hello', 'lowercase'))
 console.log(changeCase('nomayini bozza lam', 'uppercase'))
+
+// Operations
+
+const valueA = 2 + 16
+const valueB = 50 - 8 + 1
+const valueC = valueB / 2
+const value = valueA * valueC / 3
+
+console.log(value) // 129
+console.log((2 + 16) * ((50 - 8 + 1) / 2) / 3) // 129
+
+// Functions
+
+const fnA = () => { return 'Hello' }
+const fnB = () => { return 'John Smith' }
+const fnC = () => { return `your name is ${fnB()}` }
+const fn = () => { return `${fnA()}, ${fnC()}` }
+
+console.log(fn()) // Hello, your name is John Smith
+
+const english = 'Hello'
+const getAfrikaans = () => 'Goeiedag'
+const getRandom = () => Math.floor(Math.random() * 100)
+const random = getRandom ()
+
+console.log(`${english}: ${random}`);
+console.log(`${getAfrikaans ()}: ${getRandom()}`);
+console.log(`Molo: ${random}`);
+
+//Different methods for creating a function
+
+// 1. Arrow function
+const greet = () => console.log('Hello World')
+
+// 2. Function declaration
+function greet () { console.log("Hello World!") }
+
+// 3. Function Expression
+const greet = function () { console.log("Hello World!") }
