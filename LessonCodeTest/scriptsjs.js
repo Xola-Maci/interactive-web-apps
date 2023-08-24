86d08cfb783c0c7e4780538f855100ff9d73e78b
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-08-24 11:29:45 +0200

    functions tutorial

--- LessonCodeTest/scriptsjs.js
+++ LessonCodeTest/scriptsjs.js
@@ -3,3 +3,36 @@ let button = document.getElementById('changeBackground');
 button.addEventListener('click', () => {
   document.body.style.backgroundColor = 'fuchsia';
 });
+
+
+// @ts-nocheck
+
+/**
+ * @typedef {"uppercase" | "lowercase"} format - The formt as supplied to the function
+ * @typedef {"toUpperCase" | "toLowerCase"} nativeKeys - The matching key as used by JavaScript internally
+ */
+/**
+ * Converts to lowerc ase or uppercase randomly
+ * @param {string} text
+ * @param {"uppercase" | "lowercase"}[format]
+ * @returns {string}
+ */
+
+const changeCase = (text, format) => {
+  /**
+   * @type {Record<format | 'random', nativeKeys}
+   */
+  const formatMap ={
+    random: Math.random() >= 0.5 ? 'toUpperCase' : 'toLowerCase',
+    uppercase: 'toUpperCase',
+    lowercase: 'toLowerCase',
+  }
+
+  const key = format || 'random';
+  const method = formatMap [key];
+  return text[method]()
+}
+
+console.log(changeCase('Xola', 'uppercase'))
+console.log(changeCase('Hello', 'lowercase'))
+console.log(changeCase('nomayini bozza lam', 'uppercase'))
